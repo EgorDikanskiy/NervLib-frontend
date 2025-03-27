f40ace1e216db6503b849285e70b1f33d1fe3a22
0nlyssslatts <sid36205@mail.ru>
2025-03-27 19:57:57 +0300

    search component added

--- src/App/pages/CatalogPage/CatalogPage.tsx
+++ src/App/pages/CatalogPage/CatalogPage.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Catalog from './components/Catalog';
+import Search from './components/Search';
 import styles from './CatalogPage.module.scss';
 
 const CatalogPage = () => {
   return (
     <section>
+      <Search />
       <Catalog />
     </section>
   );
