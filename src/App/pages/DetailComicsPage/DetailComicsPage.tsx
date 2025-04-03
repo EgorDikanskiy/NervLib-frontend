e8b650458e507d80660170fa1262cdd472dbb876
egor_dikanskiy <egr.mr@yandex.ru>
2025-04-03 15:03:50 +0300

    connect detail with view without back

--- src/App/pages/DetailComicsPage/DetailComicsPage.tsx
+++ src/App/pages/DetailComicsPage/DetailComicsPage.tsx
@@ -33,24 +33,6 @@ const DetailComicsPage: React.FC = () => {
 
   return (
     <div>
-      <section>
-        <span className={styles.nav} onClick={() => navigate(-1)}>
-          <svg width="12" height="12" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <g clipPath="url(#clip0_137_888)">
-              <path
-                d="M1.8 5L1.45 5.35L1.1 5L1.45 4.65L1.8 5ZM10.3 9C10.3 9.13 10.25 9.26 10.15 9.35C10.06 9.45 9.93 9.5 9.8 9.5C9.67 9.5 9.54 9.45 9.45 9.35C9.35 9.26 9.3 9.13 9.3 9H10.3ZM3.95 7.85L1.45 5.35L2.15 4.65L4.65 7.15L3.95 7.85ZM1.45 4.65L3.95 2.15L4.65 2.85L2.15 5.35L1.45 4.65ZM1.8 4.5H6.8V5.5H1.8V4.5ZM10.3 8V9H9.3V8H10.3ZM6.8 4.5C7.73 4.5 8.62 4.87 9.27 5.53C9.93 6.18 10.3 7.07 10.3 8H9.3C9.3 7.34 9.04 6.7 8.57 6.23C8.1 5.76 7.46 5.5 6.8 5.5V4.5Z"
-                fill="#303030"
-              />
-            </g>
-            <defs>
-              <clipPath id="clip0_137_888">
-                <rect width="12" height="12" fill="white" />
-              </clipPath>
-            </defs>
-          </svg>
-          <p>Назад</p>
-        </span>
-      </section>
       <div className={styles.detailComicsPage}>
         <h1 className={styles.title}>{book.title}</h1>
         <img className={styles.poster} src={book.poster_url} alt={book.title} />
@@ -76,7 +58,9 @@ const DetailComicsPage: React.FC = () => {
             <strong>Дата публикации:</strong> {new Date(book.published_date).toLocaleDateString()}
           </p>
         </div>
-        <Button>Начать читать</Button>
+        <Link to={routerUrls.viewComics.create(book.slug, 1)}>
+          <Button>Начать читать</Button>
+        </Link>
       </div>
     </div>
   );
