de8dcb974cf4b2a1625b1bbfd3b051322fd9c663
0nlyssslatts <sid36205@mail.ru>
2025-04-07 18:52:08 +0300

    Избавился от лишнего рендера

--- src/App/pages/CatalogPage/CatalogPage.tsx
+++ src/App/pages/CatalogPage/CatalogPage.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import useCatalog from 'hooks/useCatalog';
+import { useSelector } from 'react-redux';
+import { RootState } from 'store';
 import Catalog from './components/Catalog';
 import Filters from './components/Filters';
 import Search from './components/Search';
@@ -7,7 +8,7 @@ import Search from './components/Search';
 import styles from './CatalogPage.module.scss';
 
 const CatalogPage = () => {
-  const { isFiltersOpen } = useCatalog();
+  const { isFiltersOpen } = useSelector((state: RootState) => state.catalog);
   return (
     <section>
       <Search />
