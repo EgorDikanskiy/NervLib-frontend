24bdb03abcf1c4122bc51f23bb427a3c312517c7
0nlyssslatts <sid36205@mail.ru>
2025-04-27 13:22:42 +0300

    Добавил стилей для компонента добавления картинки

--- src/components/ImageInput/ImageInput.tsx
+++ src/components/ImageInput/ImageInput.tsx
@@ -1,33 +1,29 @@
 import React, { useState, useRef } from 'react';
 import styles from './ImageInput.module.scss';
 const ImageInput = () => {
-  const [images, setImages] = useState<Partial<HTMLInputElement>[]>([]);
+  const [images, setImages] = useState<Partial<HTMLInputElement>>({});
   const [isDragging, setIsDragging] = useState(false);
   const fileInputRef = useRef<HTMLInputElement>(null);
 
   // Добавление и удаление
   const selectFiles = () => {
+    if (images.name) return;
     fileInputRef.current?.click();
   };
   const onFileSelect = (event: React.ChangeEvent<HTMLInputElement>) => {
     const files = event.target.files;
     if (!files) return;
     if (files?.length === 0) return;
-    for (let i = 0; i < files.length; i++) {
-      if (files[i].type.split('/')[0] !== 'image') continue;
-      if (!images.some((e) => e.name === files[i].name)) {
-        setImages((prevImages) => [
-          ...prevImages,
-          {
-            name: files[i].name,
-            src: URL.createObjectURL(files[i]),
-          },
-        ]);
-      }
+    if (files[0].type.split('/')[0] !== 'image') return;
+    if (images.name !== files[0].name) {
+      setImages({
+        name: files[0].name,
+        src: URL.createObjectURL(files[0]),
+      });
     }
   };
-  const deleteImage = (index: number) => {
-    setImages((prevImages) => prevImages.filter((_, i) => i !== index));
+  const deleteImage = () => {
+    setImages({});
   };
   // ----------------------------------------------------------------------
 
@@ -45,51 +41,48 @@ const ImageInput = () => {
     event.preventDefault();
     setIsDragging(false);
     const files = event.dataTransfer.files;
-    for (let i = 0; i < files.length; i++) {
-      if (files[i].type.split('/')[0] !== 'image') continue;
-      if (!images.some((e) => e.name === files[i].name)) {
-        setImages((prevImages) => [
-          ...prevImages,
-          {
-            name: files[i].name,
-            src: URL.createObjectURL(files[i]),
-          },
-        ]);
-      }
+    if (!files) return;
+    if (files?.length === 0) return;
+    if (files[0].type.split('/')[0] !== 'image') return;
+    if (images.name !== files[0].name) {
+      setImages({
+        name: files[0].name,
+        src: URL.createObjectURL(files[0]),
+      });
     }
   };
   // ----------------------------------------------------------------------
 
   return (
     <div className={styles.card}>
-      <div className={styles.top}>
-        <p>Drag&Drop image uploading</p>
-      </div>
-      <div className={styles.drag_area} onDragOver={onDragOver} onDragLeave={onDragLeave} onDrop={onDrop}>
-        {isDragging ? (
-          <span className={styles.select}>Drop Images Here</span>
-        ) : (
-          <>
-            Drag&Drop image here or{' '}
-            <span className={styles.select} role="button" onClick={selectFiles}>
-              Browse
-            </span>
-          </>
-        )}
+      <div
+        className={styles.drag_area}
+        onClick={selectFiles}
+        onDragOver={onDragOver}
+        onDragLeave={onDragLeave}
+        onDrop={onDrop}
+      >
+        <div className={styles.container}>
+          {images.name ? (
+            <div className={styles.image}>
+              <span className={styles.delete} onClick={() => deleteImage()}>
+                &times;
+              </span>
+              <img src={images.src} alt="" />
+            </div>
+          ) : (
+            <div className={styles.text}>
+              {isDragging ? (
+                <span className={styles.select}>Drop Images Here</span>
+              ) : (
+                <span>Перетащите изображение или нажмите здесь</span>
+              )}
+            </div>
+          )}
+        </div>
 
-        <input name="file" type="file" className={styles.file} multiple ref={fileInputRef} onChange={onFileSelect} />
+        <input name="file" type="file" className={styles.file} ref={fileInputRef} onChange={onFileSelect} />
       </div>
-      <div className={styles.container}>
-        {images.map((images, index) => (
-          <div key={index} className={styles.image}>
-            <span className={styles.delete} onClick={() => deleteImage(index)}>
-              &times;
-            </span>
-            <img src={images.src} alt="" />
-          </div>
-        ))}
-      </div>
-      <button type="button">Upload</button>
     </div>
   );
 };
