5fcbdf202e69f56df0ce3871a223db26cb8a9c78
0nlyssslatts <sid36205@mail.ru>
2025-04-27 23:06:06 +0300

    Изменил деталку под дизайн, но с обзором глав

--- src/App/pages/DetailComicsPage/DetailComicsPage.tsx
+++ src/App/pages/DetailComicsPage/DetailComicsPage.tsx
@@ -1,8 +1,9 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { Link, useParams } from 'react-router-dom';
+import { Link, useNavigate, useParams } from 'react-router-dom';
 import Loader from 'components/Loader';
 import RatingSetter from 'components/RatingSetter';
+import BackButton from 'components/ui/BackButton';
 import { Button } from 'components/ui/Button';
 import { routerUrls } from 'config/routerUrls';
 import { AppDispatch, RootState } from 'store';
@@ -11,6 +12,7 @@ import styles from './DetailComicsPage.module.scss';
 
 const DetailComicsPage: React.FC = () => {
   const dispatch = useDispatch<AppDispatch>();
+  const navigate = useNavigate();
   const { slug } = useParams<{ slug: string }>();
   const { book, chapters, loading, error } = useSelector((state: RootState) => state.detailBook);
 
@@ -39,52 +41,70 @@ const DetailComicsPage: React.FC = () => {
   }
 
   return (
-    <div>
-      <div className={styles.detailComicsPage}>
-        <h1 className={styles.title}>{book.title}</h1>
+    <div className={styles.detailComicsPage}>
+      <nav className={styles.nav}>
+        <div className={styles.backBlock}>
+          <BackButton onClick={() => navigate(routerUrls.catalog.mask)} />
+        </div>
+        <div className={styles.bookmarkBlock}>
+          <p className={styles.bookmark}>Добавить в закладки</p>
+        </div>
+      </nav>
+      <div className={styles.posterBox}>
         <img className={styles.poster} src={book.poster_url} alt={book.title} />
-        <p className={styles.description}>{book.description}</p>
+      </div>
 
-        <div className={styles.infoBlock}>
-          <p className={styles.infoItem}>
-            <strong>Возрастное ограничение:</strong> {book.age_rating}
-          </p>
-          <p className={styles.infoItem}>
-            <strong>Автор :</strong>{' '}
-            <Link to={routerUrls.public_profile.create(book.author_name)}>{book.author_name}</Link>
-          </p>
-          <p className={styles.infoItem}>
-            <strong>Главы:</strong> {book.chapter_count}
-          </p>
-          <p className={styles.infoItem}>
-            <strong>Просмотры:</strong> {book.views_count}
-          </p>
-          <p className={styles.infoItem}>
-            <strong>В избранном:</strong> {book.favourites_count}
-          </p>
-          <p className={styles.infoItem}>
-            <strong>Дата публикации:</strong> {new Date(book.published_date).toLocaleDateString()}
-          </p>
+      <div className={styles.titleBox}>
+        <h1 className={styles.title}>{book.title}</h1>
+        <p className={styles.rate}>4.7/5</p>
+      </div>
+      <div className={styles.numbers}>
+        <img src="" alt="" />
+        <p className={styles.likes}>140</p>
+        <p className={styles.favorites}>{book.favourites_count}</p>
+        <p className={styles.books}>700</p>
+      </div>
+
+      <Link to={routerUrls.viewComics.create(book.slug, chapters.length ? chapters[0].id : 1)}>
+        <Button>Читать</Button>
+      </Link>
+
+      <div className={styles.descriptionBox}>
+        <h2 className={styles.title}>Описание:</h2>
+        <p className={styles.description}>{book.description}</p>
+      </div>
+
+      <div>
+        <div className={styles.info}>
+          <h2 className={styles.title}>Дата выхода: </h2>
+          <p className={styles.infoItem}> {new Date(book.published_date).toLocaleDateString()}</p>
         </div>
-        <RatingSetter />
-        <div className={styles.chaptersList}>
-          <h2 className={styles.chaptersTitle}>Главы</h2>
-          {chapters.length ? (
-            <ul className={styles.chapterItems}>
-              {chapters.map((chapter) => (
-                <li key={chapter.id} className={styles.chapterItem}>
-                  <span className={styles.chapterName}>{chapter.title}</span>
-                  <span className={styles.chapterDate}>{new Date(chapter.published_date).toLocaleDateString()}</span>
-                </li>
-              ))}
-            </ul>
-          ) : (
-            <p className={styles.noChapters}>Главы не найдены</p>
-          )}
+        <div className={styles.info}>
+          <h2 className={styles.title}>Возрастной рейтинг:</h2>
+          <p className={styles.infoItem}>{book.age_rating}</p>
         </div>
-        <Link to={routerUrls.viewComics.create(book.slug, chapters.length ? chapters[0].id : 1)}>
-          <Button>Начать читать</Button>
-        </Link>
+        <div className={styles.tags}>
+          <h2 className={styles.title}>Теги:</h2>
+          <p className={styles.infoItem}>ниндзя</p>
+          <p className={styles.infoItem}>герои</p>
+          <p className={styles.infoItem}>сёнэн</p>
+        </div>
+      </div>
+
+      <div className={styles.chaptersList}>
+        <h2 className={styles.chaptersTitle}>Главы</h2>
+        {chapters.length ? (
+          <ul className={styles.chapterItems}>
+            {chapters.map((chapter) => (
+              <li key={chapter.id} className={styles.chapterItem}>
+                <span className={styles.chapterName}>{chapter.title}</span>
+                <span className={styles.chapterDate}>{new Date(chapter.published_date).toLocaleDateString()}</span>
+              </li>
+            ))}
+          </ul>
+        ) : (
+          <p className={styles.noChapters}>Главы не найдены</p>
+        )}
       </div>
     </div>
   );
