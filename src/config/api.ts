e4c870c915491e921e2d6f90a17e4b68adae70a6
egor_dikanskiy <egr.mr@yandex.ru>
2025-05-18 09:20:56 +0300

    edit api urls

--- src/config/api.ts
+++ src/config/api.ts
@@ -11,10 +11,10 @@ interface ApiConfigs {
 
 export const API_CONFIG: ApiConfigs = {
   development: {
-    baseURL: 'http://localhost:8000/api',
+    baseURL: 'https://backend-dev.nervlib.ru/api',
   },
   production: {
-    baseURL: 'https://your-production-api.com/api',
+    baseURL: 'https://backend-dev.nervlib.ru/api',
   },
 };
 
