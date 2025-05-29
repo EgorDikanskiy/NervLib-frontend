379700b461aa85894f0aded44d883bd8e4683143
0nlyssslatts <sid36205@mail.ru>
2025-05-29 22:27:50 +0300

    Уменьшил размер кнопок

--- src/components/ui/icons/CatalogIcon/index.tsx
+++ src/components/ui/icons/CatalogIcon/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export const CatalogIcon = (props: { active: boolean }) => {
   return (
-    <svg width="45" height="44" viewBox="0 0 45 44" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <svg width="40" height="40" viewBox="0 0 44 44" fill="none" xmlns="http://www.w3.org/2000/svg">
       <g clipPath="url(#clip0_88_132)">
         <path
           fillRule="evenodd"
